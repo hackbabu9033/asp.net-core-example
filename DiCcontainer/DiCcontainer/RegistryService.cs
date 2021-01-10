1b01b8d65b7502a52159175dcb516315c5a5075a
WIN10A\lin <hack870629@gmail.com>
2021-01-10 17:43:10 +0800

    add ch3 exercise

---
+++ DiCcontainer/DiCcontainer/RegistryService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiCcontainer
+{
+    public class RegistryService
+    {
+        public Type ServiceType { get; set; }
+        public LifeCycle LifeCycle { get; set; }
+        public Func<LifeCycle, Type[], object> ServiceFac { set; get; }
+        public RegistryService Next { set; get; }
+
+        public RegistryService(Type type, LifeCycle lifeCycle,Func<LifeCycle, Type[], object> serviceFac)
+        {
+            ServiceType = type;
+            LifeCycle = lifeCycle;
+            ServiceFac = serviceFac;
+        }
+
+    }
+}
