0545115d715e62d2451421c76e72c34e9974acd1
WIN10A\lin <hack870629@gmail.com>
2021-03-08 23:32:06 +0800

    ch10.1.4 host environment

--- ch10 host service/SimpleHostServiceExample/SimpleHostServiceExample/Program.cs
+++ ch10 host service/SimpleHostServiceExample/SimpleHostServiceExample/Program.cs
@@ -13,14 +13,19 @@ namespace SimpleHostServiceExample
         {
             var metricCollector = new MetricCollector();
             new HostBuilder()
-                .ConfigureAppConfiguration(builder=> builder.AddJsonFile("appsettings.json"))
+                // setting host configuration to set enviroment
+                .ConfigureHostConfiguration(cfgBuilder => cfgBuilder.AddCommandLine(args))
                 //1. add json file to configuration build
-                .ConfigureServices((conext,service) => service
+                .ConfigureAppConfiguration((context, builder) => builder.AddJsonFile("appsettings.json", false)
+                    .AddJsonFile(
+                        $"appsettings.{context.HostingEnvironment.EnvironmentName}.json",
+                        true))
+                .ConfigureServices((conext, service) => service
                     .AddSingleton<IGetNetworkThroughtput>(metricCollector)
                     .AddSingleton<IMemoryMetric>(metricCollector)
                     .AddSingleton<IProcessorMetric>(metricCollector)
                     // inject the aboving service dependency for hostservice PerformanceMetricLogger
-                    .AddSingleton<IMetricDeliver,MetricDeliver>()
+                    .AddSingleton<IMetricDeliver, MetricDeliver>()
                     .AddSingleton<IHostedService, PerformanceMetricLogger>()
                     .AddOptions()
                     //2. then we can use Configure method to bind Ioption and configuration file instance
